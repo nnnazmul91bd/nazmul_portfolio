89378a6f1e7b22079c5cbbc89b45ff107221bc83
MD NAZMUL ISLAM <66153355+nnnazmul91bd@users.noreply.github.com>
2020-08-08 17:14:26 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,119 @@
+
+	
+
+
+/*
+
+confirm and prompt
+
+
+	var per = prompt('Enter Your Percentage');
+	
+
+
+	if (per >= 80 && per <= 100) {
+		document.write('You are in Merit');
+	}
+	else if (per >= 60 && per <= 80) {
+		document.write('You are in 1st Division');
+	}
+	else if (per >= 45 && per <= 60) {
+		document.write('You are in 2nd Division');
+	}
+	else if (per >= 33 && per <= 45) {
+		document.write('You are in 3d Division');
+	}
+	else if (per < 33) {
+		document.write('You are in Fail');
+	}else {
+		document.write('Please Enter a Valid Percentage');
+	}
+*/
+
+
+
+
+				
+			/*
+			Ternary Operator
+
+
+			var a = 100;
+
+			var b;
+
+		 b = "Value is" +  (a > 100? "True" : "False");
+
+			document.write(b);
+			*/
+			
+
+
+
+			/*var day = 6;
+
+
+			switch (day) {
+					case 0:
+					document.write("Today is Monday");
+					
+					break;
+
+					case 1:
+					document.write("Today is Tuesday");
+					
+					break;
+
+					case 2:
+					document.write("Today is Thrusday");
+					
+					break;
+
+					case 3:
+					document.write("Today is Friday");
+					
+					break;
+
+					case 4:
+					document.write("Today is Saturday");
+					
+					break;
+
+					case 5:
+					document.write("Today is Sunday");
+					
+					break;
+
+					case 6:
+					document.write("Today is Monday");
+					
+					break;
+
+					default:
+					document.write("Enter The Valid Week  Day");
+					
+					break;
+
+			}
+*/
+
+
+
+
+
+
+
+
+
+
+				
+
+		
+
+
+
+
+
+
+
+
